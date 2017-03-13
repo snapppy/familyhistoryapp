052f4aae9b43fa52682d9c00f08f6f9016585b63
snapppy <forever.spyder@gmail.com>
2017-03-13 14:50:03 -0600

    Changes to memory

--- sandbox/memories.js
+++ sandbox/memories.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+    var test = new Memory("Lovely day in the neighborhood", "", "");
+
+    this.memories.addMemory(test);
+    this.memories.saveMemories();
+}
+
 var memories = {
     //whatever is brought in from JSON will be put here
     memoryArray: [],
@@ -12,26 +19,21 @@ var memories = {
 
     saveMemories: function() {
         //stringify JSON
-        //PUT request?
+        //POST request?
         var jsonMemories = JSON.stringify(this.memoryArray);
         var memoryRequest = new XMLHttpRequest();
-        var randomNum = Math.floor((Math.random() * 6) + 1);
 
         memoryRequest.onreadystatechange = function () {
             if (memoryRequest.readyState === 4 && memoryRequest.status === 200) {
-
-                var promptText = JSON.parse(memoryRequest.responseText);
-
-                document.getElementById("prompt").innerHTML = promptText.prompts[randomNum - 1];
             }
             else {
 
             }
         };
 
-        memoryRequest.open("PUT", "https://snapppy.github.io/familyhistoryapp/memories.json", true);
-
-        memoryRequest.send();
+        memoryRequest.open("POST", "https://snapppy.github.io/familyhistoryapp/sandbox/memories.json", true);
+        memoryRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        memoryRequest.send(jsonMemories);
     }
 }
 
